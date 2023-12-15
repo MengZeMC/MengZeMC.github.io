375ca6ad2daa820b2f3891f01bb86e71ce26e634
梦泽-MengZe <144118188+MengZeMC@users.noreply.github.com>
2023-12-15 20:01:43 +0800

    更新 footer.php

--- shoulu/footer.php
+++ shoulu/footer.php
@@ -2,4 +2,4 @@
     <p>&copy; 2023 网站收录. All Rights Reserved. Licensed under <i class="fab fa-creative-commons"></i> <i class="fab fa-cc-by"></i> <i class="fab fa-creative-commons-nc"></i> <i class="fab fa-creative-commons-sa"></i></p>
 </footer>
 
-<link rel="stylesheet" href="FontAwesome/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
